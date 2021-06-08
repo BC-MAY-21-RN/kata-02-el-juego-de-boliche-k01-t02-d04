7c821960b7706343458bebd6b20c5d305700935c
cortezroberto <cortezrobertov@gmail.com>
2021-06-08 15:22:24 -0500

    tests implemented

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,6 +1,6 @@
 class BowlingGame {
 
-    consturctor() {
+    constructor() {
         this.rolls = [];
     }
 
@@ -8,22 +8,43 @@ class BowlingGame {
         this.rolls.push(pins);
     }
 
-    get finalScore() {
+    get score() {
         let score = 0;
         let rollI = 0;
 
         for (let frameI = 0; frameI < 10; frameI++) {
+            if (this.isStrike(rollI)) {
+                score += this.strikeBonus(rollI);
+                rollI++;
+                continue;
+            }
             const frameScore = this.rolls[rollI] + this.rolls[rollI + 1];
             
-            if (frameScore === 10) {
-                score += 10 + this.rolls[rollI + 2];
+            if (this.isSpare(frameScore)) {
+                score += this.spareBonus(rollI);
             } else {
                 score+= frameScore;
-                
             }
             rollI += 2; 
         }
         return score;
+    }
+
+    isSpare(frameScore) {
+        return frameScore === 10;
+    }
+    
+    isStrike(rollI) {
+        return this.rolls[rollI] === 10;
+    }
+
+    spareBonus(rollI) {
+        return 10 + this.rolls[rollI + 2];
+    }
+
+    strikeBonus(rollI) {
+        return 10 + this.rolls[rollI + 1] + this.rolls[rollI + 2];
+
     }
 }
 
--- test/bowlingGame.test.js
+++ test/bowlingGame.test.js
@@ -1,18 +1,45 @@
-const BowlingGame = require('./bowlingGame');
+const BowlingGame = require('../src/bowlingGame');
 
 describe('Tests in BogwlingGame class', () => {
+    
     let game; //GAME
     
-    beforeEach( ()=> {
+    beforeEach( ()=> { //Before each test
         game = new BowlingGame();
     
     } );
 
     test('should return 0 for all zeros', () => {
-
-        for (let index = 0; index < 20; index++) {
-            game.roll(0);        
-        }
+        rollGame(0,20);
         expect(game.score).toEqual(0);
+    });
+    
+    test('should return 20 for all ones', () => {
+        rollGame(1,20)
+        expect(game.score).toEqual(20);
+    });
+
+    test('should return correct score for spare rolled', () => {
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+        rollGame(0,17);
+
+        expect(game.score).toEqual(16);
+    });
+
+    test('should return correct score for strike rolled', () => {
+        game.roll(10);
+        game.roll(1);
+        game.roll(1);
+        rollGame(0,17);
+
+        expect(game.score).toEqual(14);
+    });
+
+    function rollGame(pins, rolls) {
+        for (let index = 0; index < rolls; index++) {
+            game.roll(pins);
+        }
     }
-}
+});
